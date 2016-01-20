e6058b0e1337f1caeccfa7f6a5fe29ecb3a1f644
Davis, Tseng <dvsseed@hotmail.com>
2016-01-20 15:07:49 +0800

    20160120 update codes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -47,8 +47,6 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<!-- li class="yield('aactive')"><a href="/about">关于</a></li -->
-				</ul>
-				<ul class="nav navbar-nav">
 					@if(Auth::check() && Auth::user()->position != '患者' && !Auth::user()->is_admin)
 						@if(Auth::user()->position == '住院医生' || Auth::user()->position == '门诊医生')
 							<li class="@yield('activep')"><a href="/patient">患者资料</a></li>
